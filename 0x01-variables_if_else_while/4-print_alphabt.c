08d00a24605ffcf7c020678f7d8f64d9da16e656
mastermind-zed <niiadjei0169@gmail.com>
2022-06-03 06:55:29 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include < stdio.h > 
+
+int main ()
+{
+	char ch = 'a' ;
+
+	printf("Lowercase alphabets: \n") ;
+	while(ch <= 'z')
+	{
+		pritnf("%c ", ch) ;
+		ch++;
+	}
+	pritnf ("\n") ;
+
+	return 0;
+}
