4760db76084966fc396621fa7f0ac4bd9905af24
mastermind-zed <niiadjei0169@gmail.com>
2022-06-03 07:03:37 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,22 @@
-#include <stdio.h> 
+#include #include <stdio.h>
+/**
+ * main - Prints the alphabet without q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+	if (c != 'e' && c != 'q')
+	putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}<stdio.h> 
 
 int main ()
 {
