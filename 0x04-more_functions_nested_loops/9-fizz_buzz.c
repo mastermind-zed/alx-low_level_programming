fbb21235be8771d850af4671526c2f6ecb46756d
mastermind-zed <niiadjei0169@gmail.com>
2022-06-09 08:36:45 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0
  */
+
 int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
