8e8d6526b4568b5633235813000f4db720bbf899
mastermind-zed <niiadjei0169@gmail.com>
2022-06-13 08:29:47 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - Entry function
+ * @s: pointer to char
+ * Return: 0
+ */
+void print_rev(char *s)
+
+{
+	char rev[1000];
+	int i, j;
+	int length = strlen(s);
+	j = length - 1;
+	for (i = 0; i < length; ++i)
+{
+	rev[i] = s[j];
+	j--;
+	_putchar(rev[i]);
+}
+	_putchar('\n');
+}
