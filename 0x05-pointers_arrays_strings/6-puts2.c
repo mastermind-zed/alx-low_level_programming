083867af990d9072fa98a50ab242de940bd2d37a
mastermind-zed <niiadjei0169@gmail.com>
2022-06-13 09:18:19 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * put2 - prints every other character of a string
+ * @str: the string to be printed
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
