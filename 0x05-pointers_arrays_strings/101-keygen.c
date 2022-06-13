bde3d608d862408f8a244b2eb5aef4bdb7fec4d1
mastermind-zed <niiadjei0169@gmail.com>
2022-06-13 11:08:32 -0700

    101-keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - random numbers
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int i;
+	int j = 0;
+	int randnum;
+	char p[100];
+
+	srand(time(NULL));
+	for (i = 0; i < 100; i++)
+	{
+		randnum = random() % 127;
+		if (randnum > 32)
+		{
+			p[j] = randnum;
+			j++;
+		}
+	}
+	p[j] = '\0';
+	printf("%S", p);
+	return (0);
+}
