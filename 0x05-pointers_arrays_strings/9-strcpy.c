b3831aeb8fc3fee32b6516fbf90e2e67ebbb1363
mastermind-zed <niiadjei0169@gmail.com>
2022-06-13 10:43:21 -0700

    Strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * *_strcpy -  copies the string pointed to by src
  * @dest: char type string
@@ -7,14 +8,13 @@
  * the buffer pointed to by `dest`
  * Return: Pointer to `dest`
  */
+
 char *_strcpy(char *dest, char *src)
 {
-	int i = -1;
-	do 
-{
-	i++;
-	dest[i] = src[i];
-} 	
-	while (src[i] != '\0');
-	return (dest);
+int i = -1;
+do {
+i++;
+dest[i] = src[i];
+} while (src[i] != '\0');
+return (dest);
 }
