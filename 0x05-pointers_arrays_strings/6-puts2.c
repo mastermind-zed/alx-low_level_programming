0c5dff449d09ff34aa4d764c5a4bfc07dae59d81
mastermind-zed <niiadjei0169@gmail.com>
2022-06-13 09:26:34 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,14 +3,14 @@
 /**
  * puts2 - prints every other character of a string
  * @str: the string to be printed
- * 
+ *
  * Return: Always 0.
  */
 
 void puts2(char *str)
 {
 	int i = 0;
-	
+
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
