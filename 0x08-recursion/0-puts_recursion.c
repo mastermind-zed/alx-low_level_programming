2b1ef48f23c834725d7bdbf4a3217b4db25f6159
mastermind-zed <niiadjei0169@gmail.com>
2022-06-22 01:12:18 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @s: pointer to the string 
+ * @s: pointer to the string
  * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
