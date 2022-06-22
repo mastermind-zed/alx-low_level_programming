cf9719803285c10b8f5ea40f9abef3f4f597d896
mastermind-zed <niiadjei0169@gmail.com>
2022-06-22 01:23:02 -0700

    strings in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion -Entry point
+ * _print_rev_recursion - Entry point
  * @s: input
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 
-void _print_rev_recurion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
-		_print_rev_recurion(s + 1);
+		_print_rev_recursion(s + 1);
 	else
 		return;
 	_putchar(*s);
