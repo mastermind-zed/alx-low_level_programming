24b85bf2c3b700996cde765deb7cdb50b86cbc77
mastermind-zed <niiadjei0169@gmail.com>
2022-06-24 17:49:00 -0700

    done

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - function that searches a string for any of a set of bytes
+ * @s: first occurence in string
+ * @accept: any bytes in string
+ * Return: a pointer to the byte
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+				return (s + x);
+		}
+	}
+	return ('\0');
+}
