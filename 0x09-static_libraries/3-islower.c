5d062d9025bc5f2bb7042af5aed72d73c75c436a
mastermind-zed <niiadjei0169@gmail.com>
2022-06-24 17:40:44 -0700

    done

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Returns 1 if c is lowercase otherwise 0
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+	return (1);
+	}
+
+	else
+	{
+	return (0);
+	}
+}
