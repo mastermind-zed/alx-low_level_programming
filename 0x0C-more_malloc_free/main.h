c2c9d7908ea73567071b2e913a97fe86de23eaa4
mastermind-zed <niiadjei0169@gmail.com>
2022-06-30 01:25:57 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,15 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
-void free_grid(int **grid, int height);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
-int **alloc_grid(int width, int height);
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 int len(char *str);
-int num_words(char *str);
-#endif /* MAIN_H */
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
+#endif /*MAIN_H*/
