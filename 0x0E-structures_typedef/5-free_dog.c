90f07063f62afd3904974a2a253f4fa595de1b3d
mastermind-zed <niiadjei0169@gmail.com>
2022-07-04 10:53:18 -0700

    Function that frees dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,18 @@
 #include "dog.h"
+#include <stdlib.h>
 
-/** 
- * free_dog - Entry point
- * 
- * @d: dog to be freed
- * 
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer to dog_t
+ * Return: void
  */
 
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d != 0)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
 }
