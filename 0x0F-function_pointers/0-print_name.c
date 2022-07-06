bf878b2c6395aa82128286904c94a0c5598e88de
mastermind-zed <niiadjei0169@gmail.com>
2022-07-06 10:28:30 -0700

    function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - Entry point
+ * @name: name to print
+ * @f: funtion that prints name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
