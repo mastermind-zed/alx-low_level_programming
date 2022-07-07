bf7e4c4b1a22470c333a319dc979a6129bec61d6
mastermind-zed <niiadjei0169@gmail.com>
2022-07-07 14:56:34 -0700

    prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,92 +4,50 @@
 #include "variadic_functions.h"
 
 /**
- * print_char - prints a characer
- *@ap: list of arguements passed into the main function
- * Return: The passed argument
- */
-
-void print_char(va_list ap)
-{
-	printf("%c", va_arg(ap, int));
-}
-
-/**
- * print_integer - prints an integer
- *@ap: list of arguements passed into the main function
- * Return: The passed argument
- */
-
-void print_integer(va_list ap)
-{
-	printf("%d", va_arg(ap, int));
-}
-
-/**
- * print_float - prints a float
- *@ap: list of arguements passed into the main function
- * Return: The passed argument
- */
-
-void print_float(va_list ap)
-{
-	printf("%f", va_arg(ap, double));
-}
-
-/**
- * print_s - prints a string
- *@ap: list of arguements passed into the main function
- * Return: The passed argument
- */
-
-void print_s(va_list ap)
-{
-	char *s;
-
-	s = va_arg(ap, char *);
-
-	if (s  == NULL)
-		s = ("(nil)");
-	printf("%s", s);
-}
-/**
- * print_all - Prints anything
- *@format: list of arguments that can be passed through the function
- * Return: The passed argument
+ * print_all - function that prints anything
+ * @format: list of all types of arguments passed to the function
+ *
+ * Return: void
  */
 
 void print_all(const char * const format, ...)
 {
-	char *separator = "";
-	int i, k;
-	va_list ap;
+	va_list any;
+	unsigned int x;
+	char *y, *separator;
 
-	print_t list[] = {
-		{"c",  print_char},
-		{"i", print_integer},
-		{"f", print_float},
-		{"s", print_s},
-		{NULL, NULL},
-	};
+	va_start(any, format);
 
-	va_start(ap, format);
-	i = 0;
-	while ((format != NULL) && (format[i] != '\0'))
+	separator = "";
+
+	x = 0;
+	while (format && format[x])
 	{
-		k = 0;
-		while (list[k].name != NULL)
+		switch (format[x])
 		{
-			if (*(list[k].name) == format[i])
-			{
-				printf("%s", separator);
-				list[k].type(ap);
-				separator = ", ";
-			}
-			k++;
+			case 'c':
+				printf("%s%c", separator, va_arg(any, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(any, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(any, double));
+				break;
+			case 's':
+				y = va_arg(any, char *);
+				if (y == NULL)
+					y = "(nil)";
+				printf("%s%s", separator, y);
+				break;
+			default:
+				x++;
+				continue;
 		}
-		i++;
-		k = 0;
+		separator = ", ";
+		x++;
 	}
+
 	printf("\n");
-	va_end(ap);
+	va_end(any);
 }
