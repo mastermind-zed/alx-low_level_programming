506f56efdbfec02051a15d7657fcb8cfe7b2af51
mastermind-zed <niiadjei0169@gmail.com>
2022-07-18 08:50:26 -0700

    added

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Entry point
+ * @head: pointer to structure
+ * Return: add
+ */
+
+int sum_listint(listint_t *head)
+{
+	int add = 0;
+
+	if (head)
+	{
+		while (head)
+		{
+			add += head->n;
+			head = head->next;
+		}
+		return (added);
+	}
+	else
+		return (0);
+}
